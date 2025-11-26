1fdedce436c54586d1e80a37d86fc900aa34bd2f
mateusz-gluch <mateusz.gluch@elmodis.com>
2025-11-26 19:50:02 +0100

    feat: json support for Shared Vec

--- include/objects/SharedVector.h
+++ include/objects/SharedVector.h
@@ -6,6 +6,7 @@
 #define PROJEKT_SHAREDVECTOR_H
 
 #include <stdexcept>
+#include <nlohmann/json.hpp>
 
 template<typename T>
 class SharedVector {
@@ -36,18 +37,52 @@ class SharedVector {
         const T* begin() const { return reinterpret_cast<const T*>(this + 1); }
         const T* end() const   { return begin() + _size; }
 
-        void push_back(const T& item) {
+        void push_back(const T &item) {
             if (_size >= _capacity) throw std::runtime_error("full");
-            this[_size++] = item;
+            (*this)[_size++] = item;
         }
 
-        void erase(size_t index) {
-            if (index >= _size) throw std::out_of_range("index out of range");
-            for (size_t i = index + 1; i < _size; ++i)
-                this[i - 1] = this()[i];
+        void erase(T* it) {
+            // check range
+            if (it < begin() || it >= end())
+                throw std::out_of_range("iterator out of range");
+
+            // move content one step back
+            for (T* i = it + 1; i < end(); ++i) {
+                *(i - 1) = *i;
+            }
+
+            // decrease nominal size
             --_size;
         }
+
+        friend void from_json(const nlohmann::json &j, SharedVector<T> &vec);
+        friend void to_json(nlohmann::json &j, const SharedVector<T> &vec);
 };
 
+template<typename T>
+void to_json(nlohmann::json& j, const SharedVector<T>& vec) {
+    j = nlohmann::json::array();
+    const T* data = vec.begin();
+    for (size_t i = 0; i < vec._size; ++i) {
+        j.push_back(data[i]);
+    }
+}
+
+template<typename T>
+void from_json(const nlohmann::json& j, SharedVector<T>& vec) {
+    if (!j.is_array())
+        throw std::runtime_error("JSON is not an array for SharedVector");
+
+    if (j.size() > vec._capacity)
+        throw std::runtime_error("JSON array too large for SharedVector capacity");
+
+    vec._size = j.size();
+    T* data = vec.begin();
+
+    for (size_t i = 0; i < vec._size; ++i) {
+        data[i] = j[i].get<T>();
+    }
+}
 
 #endif //PROJEKT_SHAREDVECTOR_H
