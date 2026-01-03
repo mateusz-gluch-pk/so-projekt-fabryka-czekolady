16d3c5f79f99167374b6f6f452cbea9d52c24ae6
mateusz-gluch-pk <mateusz.gluch@student.pk.edu.pl>
2026-01-03 18:06:31 +0100

    feat: Control Panel

--- include/ui/ControlPanel.h
+++ include/ui/ControlPanel.h
@@ -5,16 +5,41 @@
 #ifndef FACTORY_CONTROLPANEL_H
 #define FACTORY_CONTROLPANEL_H
 #include "Supervisor.h"
-#include "../../cmake-build-debug/_deps/ftxui-src/include/ftxui/component/component_base.hpp"
-#include "../../cmake-build-debug/_deps/ftxui-src/src/ftxui/dom/box_helper.hpp"
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/loop.hpp"
+
 
 class ControlPanel {
-    explicit ControlPanel(Supervisor& sv): _supervisor(sv) {}
+public:
+    explicit ControlPanel(Supervisor& sv): _supervisor(sv) {
+        auto stop_workers = ftxui::Button(
+            "Stop Workers",
+            [&]{sv.stop_workers();}
+            );
 
-    ftxui::Component
-    private:
+        auto stop_deliverers = ftxui::Button(
+            "Stop Deliverers",
+            [&]{sv.stop_deliverers();}
+            );
 
+        auto stop_warehouses = ftxui::Button(
+            "Stop Warehouses",
+            [&]{sv.stop_warehouses();}
+            );
+
+        auto stop_all = ftxui::Button(
+            "Stop Simulation",
+            [&]{sv.stop_all();}
+            );
+
+        _component = ftxui::Container::Vertical({stop_workers, stop_deliverers, stop_warehouses, stop_all});
+    }
+
+    ftxui::Component component() {return _component;};
+
+private:
     Supervisor& _supervisor;
+    ftxui::Component _component;
 };
 
 
