38f9afae72a4a9e0220290c0e7cc258f95dc24b8
mateusz-gluch-pk <mateusz.gluch@student.pk.edu.pl>
2026-01-06 14:49:08 +0100

    feat: main

--- include/ui/Layout.h
+++ include/ui/Layout.h
@@ -5,9 +5,39 @@
 #ifndef FACTORY_LAYOUT_H
 #define FACTORY_LAYOUT_H
 
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+
 
 class Layout {
-};
+public:
+    Layout(ftxui::Component dashboard, ftxui::Component control_panel, ftxui::Component log_panel):
+    _dashboard(dashboard), _control_panel(control_panel), _log_panel(log_panel)
+    {
+        // --- Compose top row (Dashboard | ControlPanel) ---
+        _top_row = ftxui::Container::Horizontal({
+            _dashboard |
+            ftxui::flex, _control_panel
+        });
 
+        // --- Compose root layout (top row + logs) ---
+        _root = ftxui::Container::Vertical({
+            _top_row | ftxui::flex,
+            _log_panel | ftxui::size(ftxui::HEIGHT, ftxui::EQUAL, 7)
+        });
+    }
+
+    ftxui::Component component() {
+        return _root;
+    }
+
+private:
+    ftxui::Component _dashboard;
+    ftxui::Component _control_panel;
+    ftxui::Component _log_panel;
+
+    ftxui::Component _top_row;
+    ftxui::Component _root;
+};
 
 #endif //FACTORY_LAYOUT_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,107 @@
 // Created by mateusz on 13.12.2025.
 //
 
-int main () {
+#include "Supervisor.h"
+#include "services/DelivererService.h"
+#include "services/LoggerService.h"
+#include "services/WarehouseService.h"
+#include "services/WorkerService.h"
+#include "ui/ControlPanel.h"
+#include "ui/Dashboard.h"
+#include "ui/Layout.h"
+#include "ui/LogPanel.h"
+
+#include <ftxui/component/screen_interactive.hpp>
+
+
+int main() {
+    // Setup Logger
+    LoggerService logger("factory-simulation", INFO);
+    Logger log = logger.get();
+
+    // Setup Services
+    WarehouseService warehouses(log);
+    WorkerService workers(log);
+    DelivererService deliverers(log);
+    ShutdownToken run;
+
+    // Supervisor
+    Supervisor sv(warehouses, deliverers, workers, run, log);
+
+
+    // Setup Warehouses
+    // WH 1 - with capacity
+    constexpr int capacity = 100;
+    auto ingredients = warehouses.create("ingredients", capacity);
+    if (ingredients == nullptr) {
+        return 1;
+    }
+
+    // WH 2 - with large capacity
+    constexpr int output_capacity = 1000000;
+    auto outputs = warehouses.create("outputs", output_capacity);
+    if (outputs == nullptr) {
+        return 1;
+    }
+
+    // Setup Deliverers
+    // D1 - Item A
+    ItemTemplate a("A", 1, 100);
+    deliverers.create("deliverer-a", a, *ingredients);
+
+    // D2 - Item B
+    ItemTemplate b("B", 1, 100);
+    deliverers.create("deliverer-b", b, *ingredients);
+
+    // D3 - Item C
+    ItemTemplate c("C", 2, 100);
+    deliverers.create("deliverer-c", c, *ingredients);
+
+    // D4 - Item D
+    ItemTemplate d("D", 3, 100);
+    deliverers.create("deliverer-d", d, *ingredients);
+
+    // Setup Workers
+    // W1 - {A, B, C} -> T1
+    std::vector<Item> r1_in{
+        {"A", 1, 1},
+        {"B", 1, 1},
+        {"C", 2, 1},
+    };
+    Recipe r1(r1_in, {"T1", 1, 1});
+    workers.create("worker-t1", r1, *ingredients, *outputs);
+
+    // W2 - {A, B, D} -> T2
+    std::vector<Item> r2_in{
+            {"A", 1, 1},
+            {"B", 1, 1},
+            {"D", 3, 1},
+        };
+    Recipe r2(r1_in, {"T1", 1, 1});
+    workers.create("worker-t2", r1, *ingredients, *outputs);
+
+    // Setup UI
+    auto control_panel = std::make_shared<ControlPanel>(sv);
+    auto log_panel = std::make_shared<LogPanel>(logger);
+
+    auto workers_table = std::make_shared<WorkerTable>(workers);
+    auto warehouses_table = std::make_shared<WarehouseTable>(warehouses);
+    auto deliverers_table = std::make_shared<DelivererTable>(deliverers);
+    auto dashboard = std::make_shared<Dashboard>(
+      warehouses_table->component(),
+      workers_table->component(),
+      deliverers_table->component()
+    );
+
+    auto layout = std::make_shared<Layout>(
+        dashboard->component(),
+        control_panel->component(),
+        log_panel->component()
+    );
+
+    // --- Start terminal UI ---
+    auto screen = ftxui::ScreenInteractive::TerminalOutput();
+    screen.Loop(layout->component());
+
     return 0;
 }
